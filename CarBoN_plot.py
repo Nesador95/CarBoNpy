8c1a96266b1e58c880e163ef46145d8059c0f2da
Nesador95 <42779085+Nesador95@users.noreply.github.com>
2018-10-18 21:19:51 -0400

    Add files via upload

--- CarBoN_plot.py
+++ CarBoN_plot.py
@@ -7,17 +7,28 @@
 #usage			:python CarBoN_plot.py
 #notes			:
 #python_version		:3.5.1
-#==============================================================================
+#=============================================================================
 
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.ticker as ticker
+
+import datainput
 #import matplotlib._color_data 
 #import matplotlib.cm as cmx
 #from sys import exit
+file_format, species_file, reactions_file, output_file, model_type, density, Tinit, start_time, end_time, outfile = datainput.settings()
+##############################################################################
+# The following is used to specify the data file to be plotted
+# write its path as "output/filename.dat"  
+##############################################################################
 
-outfile = 'basic_C-0.01_O-1_Si-0.1_Compton_D1e10_T1.8e4.dat'
-infile = np.load(outfile+".npz")
+#file = str('' + outfile + '')
+infile = np.load( outfile )
+
+##############################################################################
+# The following will take care of the plot's setup
+############################################################################## 
 
 totmass = 1.11 ### Total of C+O+Si
 
@@ -25,44 +36,43 @@ plottitle = 'C/O = 1/100      C/Si = 1/10'
 
 print(infile.files)
 
-y=infile['y']/totmass
+y = infile['y'] / totmass
 speciesidx = infile['speciesidx'][()]  #extracts dictionary from array?
-speciesmass =infile['speciesmass'][()]
-abundance=infile['abundance']
-time=365.25*(infile['time'])#-(99/365.25))
+speciesmass = infile['speciesmass'][()]
+abundance = infile['abundance']
+time = 365.25 * (infile['time'])         #-(99/365.25))
 
 #print(speciesidx)
 #print(y[-1,11])
 
-######
-# Tests for mass conservation. Total must = 1. 
-######
+##############################################################################
+# The following tests for mass conservation. Total must = 1. 
+##############################################################################
 
-total=0
-for name,index in speciesidx.items():
-    if index!=99:
-        total += speciesmass[name]*y[:,index]
+total = 0
+for name, index in speciesidx.items():
+    if index != 99:
+        total += speciesmass[name] * y[:,index]
 
 #exit()
 
-colors=['xkcd:red','xkcd:blue','xkcd:green','xkcd:cyan','xkcd:magenta',\
-'xkcd:goldenrod','xkcd:orange','xkcd:indigo']
 
-######
-# Figure Plotting
-######
+##############################################################################
+# The following is the figure Plotting
+##############################################################################
+
 fig = plt.figure(figsize=(10, 10))
 ax1 = fig.add_subplot(111)
 
-ax1.xkcd()
-ax1.plot(time,y[:,4],label='$\mathrm{CO}$',linewidth=2.0,color=colors[0])
-ax1.plot(time,y[:,5],label='$\mathrm{C_2}$',linewidth=2.0,color=colors[1])
-ax1.plot(time,y[:,6],label='$\mathrm{C_3}$',linewidth=2.0,color=colors[2])
-ax1.plot(time,y[:,14],label='$\mathrm{O_2}$',linewidth=2.0,color=colors[3])
-ax1.plot(time,y[:,12],'--',label='$\mathrm{SiC}$',linewidth=2.0,color=colors[4])
-ax1.plot(time,y[:,13],'--',label='$\mathrm{SiO}$',linewidth=2.0,color=colors[5])
-ax1.plot(time,y[:,15],'-.',label='$\mathrm{(SiC)_2}$',linewidth=2.0,color=colors[6])
-ax1.plot(time,y[:,16],'-.',label='$\mathrm{(SiO)_2}$',linewidth=2.0,color=colors[7])
+
+ax1.plot(time, y[:, 4], label = '$\mathrm{CO}$', linewidth = 2.0)
+ax1.plot(time, y[:, 5], label = '$\mathrm{C_2}$', linewidth = 2.0)
+ax1.plot(time, y[:, 6], label = '$\mathrm{C_3}$', linewidth = 2.0)
+ax1.plot(time, y[:, 14], label = '$\mathrm{O_2}$', linewidth = 2.0)
+ax1.plot(time, y[:, 12], '--', label = '$\mathrm{SiC}$', linewidth = 2.0)
+ax1.plot(time, y[:, 13], '--', label = '$\mathrm{SiO}$', linewidth = 2.0)
+ax1.plot(time, y[:, 15], '-.', label = '$\mathrm{(SiC)_2}$', linewidth = 2.0)
+ax1.plot(time, y[:, 16], '-.', label = '$\mathrm{(SiO)_2}$', linewidth = 2.0)
 
 #ax1.plot(time,total,label='Test',color='black')
         
@@ -76,15 +86,16 @@ ax1.plot(time,y[:,16],'-.',label='$\mathrm{(SiO)_2}$',linewidth=2.0,color=colors
 #    if index in [17,18,19,20]:
 #        ax1.plot(time,y[:,index],ls='dashdot',label=name,linewidth=2.0) 
 
-plt.subplots_adjust(left=0.1, right=0.84, top=0.9, bottom=0.1,wspace=0.5) 
-ax1.set_ylim([1e-20,2e-2])
+plt.subplots_adjust(left = 0.1, right = 0.84,
+                    top = 0.9, bottom = 0.1, wspace = 0.5) 
+ax1.set_ylim([1e-20, 2e-2])
 #ax1.set_xlim([0,200])
-ax1.set_xlim([100,1760])
+ax1.set_xlim([100, 1760])
 ax1.set_yscale('log')
 ax1.set_xlabel('t-t0 (days)')
 ax1.xaxis.set_major_locator(ticker.MultipleLocator(100))
 ax1.set_ylabel('Abundance')
-ax1.legend(loc='center left', bbox_to_anchor=(1, 0.5))
+ax1.legend(loc='center left', bbox_to_anchor = (1, 0.5))
 ax1.set_title(plottitle)
 plt.show()
 
--- datainput.py
+++ datainput.py
@@ -12,7 +12,8 @@ import pandas as pd
 import configparser as cp
 
 """
-This function reads in a KIDA reactions and species file using Pandas. Column names and widths are provided by KIDA.
+This function reads in a KIDA reactions and species file using Pandas.
+Column names and widths are provided by KIDA.
 
 Outputs:  
 
@@ -22,16 +23,43 @@ speciesmass - dictionary of species name and mass*
 numspecies - Total number of chemical species + a "catchall" zero.
 
 
-*Mass is calculated per atom, i.e. O2 has a mass of 2 and Si2O2 has a mass of 4. 
+*Mass is calculated per atom, i.e. O2 has a mass of 2
+and Si2O2 has a mass of 4. 
 
-Big Thanks to: http://stackoverflow.com/questions/40663510/pandas-read-fwf-ignoring-values
+Big Thanks to: 
+http://stackoverflow.com/questions/40663510/pandas-read-fwf-ignoring-values
+
 """
+##############################################################################
+# The following loads the imput from a KIDA file
+##############################################################################
 
 def kida_input(reac_file,spec_file):
+    """
+    This function reads in a KIDA reactions and species file using Pandas.
+    Column names and widths are provided by KIDA.
+
+    Outputs:  
+
+    reactions - dataframe for all reactions in the network
+    speciesidx - dictionary of species name and index
+    speciesmass - dictionary of species name and mass*
+    numspecies - Total number of chemical species + a "catchall" zero.
+
+
+    *Mass is calculated per atom, i.e. O2 has a mass of 2
+    and Si2O2 has a mass of 4. 
+
+    Big Thanks to: 
+    http://stackoverflow.com/questions/40663510/pandas-read-fwf-ignoring-values
+
+    """
     speciesidx={}
     speciesmass={}
-    colnames=["Input1","Input2","Input3","Output1","Output2","Output3","","","alpha","beta","gamma","F","g",\
-                  "Type_of_uncertainty","itype","T_low","T_high","Formula","Number","Numbers of","Recommendation"]
+    colnames=["Input1", "Input2", "Input3", "Output1", "Output2", "Output3", 
+              "", "", "alpha", "beta", "gamma", "F", "g",
+              "Type_of_uncertainty", "itype", "T_low", "T_high", "Formula",
+              "Number", "Numbers of", "Recommendation"]
     colwidths=[11,11,12,11,11,11,11,12,11,11,11,9,9,5,3,7,7,3,5,2,3]
     collist=[0,1,2,3,4,5,8,9,10,11,12,13,14,15,16,17,18,19,20]
     with open(spec_file,'r') as my_file:
@@ -48,32 +76,39 @@ def kida_input(reac_file,spec_file):
 
     convert = lambda x: speciesidx[x] if x!='' else None
 
-    reactions=pd.read_fwf(reac_file,comment='#',names=colnames,widths=colwidths,usecols=collist,\
-                              converters={0:convert,1:convert,2:convert,3:convert,4:convert,5:convert})
-    reactions.fillna(0,inplace=True)
+    reactions=pd.read_fwf(reac_file, comment='#', names = colnames,
+                          widths = colwidths, usecols = collist,
+                          converters = {0:convert, 1:convert, 2:convert,
+                                        3:convert,4:convert,5:convert})
+    reactions.fillna(0,inplace = True)
     reactions[colnames[0:6]] = reactions[colnames[0:6]].astype(int)
     
     return reactions,speciesidx,speciesmass,numspecies
 
-"""
-This function reads in "basic format" reactions/species files using Pandas. 
-Basic Reaction Format is a comma delimited file. 
-
-Input1 Input2 Input3 Output1 Output2 Output3 Alpha Beta Gamma Formula
-
-For all Arrhenius reactions, Formula should be 3.
-
-Basic Species Format:
-
-Name Mass Index
-
-This probably needs more documentation. 
-"""
+##############################################################################
+# The following loads the imput from a BASIC file
+##############################################################################
 
 def basic_input(reac_file,spec_file):
+    """
+    This function reads in "basic format" reactions/species files using Pandas. 
+    Basic Reaction Format is a comma delimited file. 
+
+    Input1 Input2 Input3 Output1 Output2 Output3 Alpha Beta Gamma Formula
+
+    For all Arrhenius reactions, Formula should be 3.
+
+    Basic Species Format:
+
+    Name Mass Index
+
+    This probably needs more documentation. 
+
+    """
     speciesidx={}
     speciesmass={}
-    colnames=["Input1","Input2","Input3","Output1","Output2","Output3","alpha","beta","gamma","Formula"]
+    colnames=["Input1", "Input2", "Input3", "Output1", "Output2", "Output3",
+              "alpha", "beta", "gamma", "Formula"]
     with open(spec_file,'r') as my_file:
         for line in my_file:
             columns = line.strip().split()
@@ -84,20 +119,27 @@ def basic_input(reac_file,spec_file):
 
     convert = lambda x: speciesidx[x] if x!=' ' else None
 
-    reactions=pd.read_csv(reac_file,comment='#',names=colnames,converters={0:convert,1:convert,2:convert,3:convert,4:convert,5:convert})
-    reactions.fillna(0,inplace=True)
+    reactions=pd.read_csv(reac_file, comment = '#', names = colnames,
+                          converters = {0:convert, 1:convert, 2:convert,
+                                        3:convert, 4:convert, 5:convert})
+    reactions.fillna(0,inplace = True)
     reactions[colnames[0:6]] = reactions[colnames[0:6]].astype(int)
     
     return reactions,speciesidx,speciesmass,numspecies
 
-"""
-This function prints the entire Pandas dataframe to test. Make sure that your terminal window is wide enough!
-
-Code shamlessly absorbed from stackoverflow
-http://stackoverflow.com/questions/19124601/is-there-a-way-to-pretty-print-the-entire-pandas-series-dataframe
-"""
+##############################################################################
+# The following prints the entire list straight from pandas
+##############################################################################
 
 def print_full(x):
+    """
+    This function prints the entire Pandas dataframe to test. 
+    Make sure that your terminal window is wide enough!
+
+    Code shamlessly absorbed from stackoverflow
+    http://stackoverflow.com/questions/19124601/is-there-a-way-to-pretty-print-the-entire-pandas-series-dataframe
+
+    """
     pd.set_option('display.max_rows', len(x))
     pd.set_option('display.max_columns', 1000)
     pd.set_option('display.expand_frame_repr', False)
@@ -106,16 +148,20 @@ def print_full(x):
     pd.reset_option('display.max_columns')
     pd.reset_option('display.expand_frame_repr')
 
-"""
-This function reads the settings.ini file. 
-"""
+##############################################################################
+# The following reads the settings file
+##############################################################################
 
 def settings():
+    
+    """ This function reads the settings specified in settings.ini """
+
     config = cp.ConfigParser()
     config.read('settings.ini')
     
-    files=config['files']
-    model=config['model']
+    files = config['files']
+    model = config['model']
+    plot = config['plot']
     
     file_format = files['format']
     species_file = 'data/' + files['species file']
@@ -124,19 +170,25 @@ def settings():
     model_type = model['model type']
     density = model.getfloat('density')
     temperature = model.getfloat('temperature')
+    start_time = model.getfloat('start time')
+    end_time = model.getfloat('end time')
+    outfile = plot['outfile for plotting']
 
-    return file_format,species_file,reactions_file,output_file,model_type,density,temperature
+    return file_format, species_file, reactions_file, output_file, model_type, density, temperature, start_time, end_time, outfile
 
-"""
-This function reads the abundances.ini file
-"""
+##############################################################################
+# The following reads the abundances file
+##############################################################################
 
 def abundances():
+
+    """ This function reads the abundances.ini file """
+    
     initial = {}
     with open('abundances.ini','r') as my_file:
         header = my_file.readline()
         for line in my_file:
             columns = line.strip().split()
-            initial[columns[0]]=float(columns[1])
+            initial[columns[0]] = float(columns[1])
             
     return initial
